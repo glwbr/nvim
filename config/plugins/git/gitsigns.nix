2a8218707cc10edd9006164fee85b3a52030f968
Glauber Santana <glauber.silva14@gmail.com>
2024-11-24 16:42:13 -0300

    fix(git): update git blame format

--- config/plugins/git/gitsigns.nix
+++ config/plugins/git/gitsigns.nix
@@ -5,7 +5,7 @@
       enable = true;
       settings = {
         current_line_blame = true;
-        current_line_blame_formatter = " <author> • <author_time:%R> – <summary>";
+        current_line_blame_formatter = " <author> • <author_time:%R> <summary>";
         current_line_blame_formatter_nc = " Uncommitted changes";
         signs = {
           add.text = icons.GitSign;
