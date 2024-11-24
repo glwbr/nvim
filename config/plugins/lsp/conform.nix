41ce83306da6a6d78660e854b1c133077ebac27b
Glauber Santana <glauber.silva14@gmail.com>
2024-11-24 17:16:22 -0300

    fix(conform): correctly use the packaged formatters

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -1,48 +1,93 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  extraPackages = with pkgs; [
-    gofumpt
-    golines
-    jq
-    nixfmt-rfc-style
-    phpPackages.php-cs-fixer
-    prettierd
-    ruff
-    stylua
-  ];
-
   plugins.conform-nvim = {
     enable = true;
     settings = {
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
       log_level = "error";
       notify_on_error = true;
       notify_no_formatters = true;
 
       formatters_by_ft = {
-        css = [ "prettierd" ];
-        go = [
-          "gofumpt"
-          "golines"
+        "_" = [ "trim_whitespace" ];
+        css = [
+          "prettierd"
+          "prettier"
         ];
-        html = [ "prettierd" ];
-        javascript = [ "prettierd" ];
+        go = [
+          "goimports"
+          "golines"
+          "gofumpt"
+        ];
+        html = [
+          "prettierd"
+          "prettier"
+        ];
+        javascript = [
+          "prettierd"
+          "prettier"
+        ];
+        javascriptreact = [
+          "prettierd"
+          "prettier"
+        ];
         json = [ "jq" ];
         lua = [ "stylua" ];
         markdown = [ "prettierd" ];
         nix = [ "nixfmt" ];
-        php = [ "php_cs_fixer" ];
+        php = [
+          "phpstan"
+          "php_cs_fixer"
+        ];
         python = [
           "ruff_format"
           "ruff_fix"
         ];
-        typescript = [ "prettierd" ];
-        yaml = [ "prettierd" ];
-        "_" = [ "trim_whitespace" ];
+        typescript = [
+          "prettierd"
+          "prettier"
+        ];
+        typescriptreact = [
+          "prettierd"
+          "prettier"
+        ];
+        yaml = [
+          "prettierd"
+          "prettier"
+        ];
       };
 
-      format_on_save = {
-        lspFallback = true;
-        timeoutMs = 500;
+      formatters = {
+        jq = {
+          command = "${lib.getExe pkgs.jq}";
+        };
+        golines = {
+          command = "${lib.getExe' pkgs.golines "golines"}";
+        };
+        gofumpt = {
+          command = "${lib.getExe pkgs.gofumpt}";
+        };
+        nixfmt = {
+          command = "${lib.getExe pkgs.nixfmt-rfc-style}";
+        };
+        phpstan = {
+          command = "${lib.getExe pkgs.php84Packages.phpstan}";
+        };
+        php_cs_fixer = {
+          command = "${lib.getExe pkgs.php84Packages.php-cs-fixer}";
+        };
+        prettierd = {
+          command = "${lib.getExe pkgs.prettierd}";
+        };
+        ruff = {
+          command = "${lib.getExe pkgs.ruff}";
+        };
+        stylua = {
+          command = "${lib.getExe pkgs.stylua}";
+        };
       };
     };
   };
