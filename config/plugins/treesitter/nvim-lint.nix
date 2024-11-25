998fc95734a70bcdb02d76dd5f9c432beedbb135
Glauber Santana <glauber.silva14@gmail.com>
2024-11-24 23:40:26 -0300

    chore: add yamllint remove phpstan

--- config/plugins/treesitter/nvim-lint.nix
+++ config/plugins/treesitter/nvim-lint.nix
@@ -3,28 +3,24 @@
   extraPackages = with pkgs; [
     golangci-lint
     eslint_d
-    hadolint
     nodePackages.jsonlint
-    phpPackages.phpstan
     selene
     statix
-    stylelint
+    yamllint
   ];
 
   plugins.lint = {
     enable = true;
     lintersByFt = {
-      css = [ "stylelint" ];
-      docker = [ "hadolint" ];
       go = [ "golangci-lint" ];
       javascript = [ "eslint_d" ];
       javascriptreact = [ "eslint_d" ];
       json = [ "jsonlint" ];
       lua = [ "selene" ];
       nix = [ "statix" ];
-      php = [ "phpstan" ];
       typescript = [ "eslint_d" ];
       typescriptreact = [ "eslint_d" ];
+      yaml = [ "yamllint" ];
     };
   };
 }
