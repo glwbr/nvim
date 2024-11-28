c6c99e057df29eb068c20cf0d179ccd6e5d3935c
Glauber Santana <glauber.silva14@gmail.com>
2024-11-27 21:37:27 -0300

    feat: update lsp settings

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -1,4 +1,8 @@
-_: {
+_:
+let
+  flake = builtins.getEnv "FLAKE";
+in
+{
   plugins = {
     lsp-format.enable = true;
     lsp = {
@@ -28,19 +32,67 @@ _: {
         cssls.enable = true;
         dockerls.enable = true;
         docker_compose_language_service.enable = true;
-        elixirls.enable = true;
+        # elixirls.enable = true;
+        eslint.enable = true;
         gopls.enable = true;
         html.enable = true;
-        htmx.enable = true;
+        # htmx.enable = true;
         jsonls.enable = true;
         lua_ls.enable = true;
         marksman.enable = true;
         nginx_language_server.enable = true;
-        nixd.enable = true;
+        nixd = {
+          enable = true;
+          extraOptions.settings = {
+            nixd = {
+              nixpkgs.expr = ''import (builtins.getFlake "${flake}").inputs.nixpkgs { }'';
+              options = {
+                nixos.expr = ''(builtins.getFlake "${flake}").nixosConfigurations.sonata.options'';
+                home_manager.expr = ''(builtins.getFlake "${flake}").homeConfigurations.sonta.options'';
+              };
+              flake_parts.expr = ''let flake = builtins.getFlake ("${flake}"); in flake.debug.options // flake.currentSystem.options'';
+            };
+          };
+        };
         phpactor.enable = true;
         ruff.enable = true;
         templ.enable = true;
-        ts_ls.enable = true;
+        ts_ls = {
+          enable = true;
+          filetypes = [
+            "javascript"
+            "javascriptreact"
+            "typescript"
+            "typescriptreact"
+            "vue"
+          ];
+          settings = {
+            init_options = {
+              plugins = {
+                name = "@vue/typescript-plugin";
+                languages = [
+                  "javascript"
+                  "typescript"
+                  "vue"
+                ];
+                location = "arbitrary_location";
+              };
+            };
+            typescript = {
+              inlayHints = {
+                enumMemberValues.enabled = true;
+                functionLikeReturnTypes.enabled = true;
+                parameterNames.enabled = "literals";
+                parameterTypes.enabled = true;
+                propertyDeclarationTypes.enabled = true;
+                variableType.enabled = false;
+              };
+
+              updateImportsOnFileMove.enabled = "always";
+              suggest.completeFunctionCalls = true;
+            };
+          };
+        };
         volar.enable = true;
         yamlls = {
           enable = true;
