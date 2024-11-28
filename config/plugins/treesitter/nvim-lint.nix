ee2e8474209a1242823e2eb5d8a21b3a26a638d1
Glauber Santana <glauber.silva14@gmail.com>
2024-11-27 21:37:54 -0300

    chore: remove extraPackages use lib.getExe

--- config/plugins/treesitter/nvim-lint.nix
+++ config/plugins/treesitter/nvim-lint.nix
@@ -1,18 +1,10 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  extraPackages = with pkgs; [
-    golangci-lint
-    eslint_d
-    nodePackages.jsonlint
-    selene
-    statix
-    yamllint
-  ];
-
   plugins.lint = {
     enable = true;
+
     lintersByFt = {
-      go = [ "golangci-lint" ];
+      docker = [ "hadolint" ];
       javascript = [ "eslint_d" ];
       javascriptreact = [ "eslint_d" ];
       json = [ "jsonlint" ];
@@ -21,6 +13,27 @@
       typescript = [ "eslint_d" ];
       typescriptreact = [ "eslint_d" ];
       yaml = [ "yamllint" ];
+    };
+
+    linters = {
+      eslint_d = {
+        cmd = lib.getExe pkgs.eslint_d;
+      };
+      hadolint = {
+        cmd = lib.getExe pkgs.hadolint;
+      };
+      jsonlint = {
+        cmd = lib.getExe' pkgs.nodePackages.jsonlint "jsonlint";
+      };
+      selene = {
+        cmd = lib.getExe pkgs.selene;
+      };
+      statix = {
+        cmd = lib.getExe pkgs.statix;
+      };
+      yamllint = {
+        cmd = lib.getExe pkgs.yamllint;
+      };
     };
   };
 }
