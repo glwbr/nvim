de168582c373b2480688fc80d824c988ca6cd77f
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-02-08 21:55:58 -0300

    feat(plugins): add nvim-lint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,14 +3,18 @@ return {
   event = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
   config = function()
     local lint = require 'lint'
+
     lint.linters_by_ft = {
-      javascript = { 'eslint_d' },
-      javascriptreact = { 'eslint_d' },
-      lua = { 'selene' },
-      markdown = { 'markdownlint' },
-      nix = { 'nix' },
       typescript = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
+      javascript = { 'eslint_d' },
+      javascriptreact = { 'eslint_d' },
     }
+
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePre', 'InsertLeave', 'TextChanged' }, {
+      callback = function()
+        require('lint').try_lint()
+      end,
+    })
   end,
 }
