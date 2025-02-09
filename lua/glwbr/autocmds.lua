d818e48cd7c9498e15fbe86e4b01238c4fc02694
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-02-08 23:24:33 -0300

    feat(autocmds): add close_with_esc

--- lua/glwbr/autocmds.lua
+++ lua/glwbr/autocmds.lua
@@ -12,6 +12,38 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
   end,
 })
 
+-- close some filetypes with <Esc>
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'close_with_esc',
+  pattern = {
+    'PlenaryTestPopup',
+    'checkhealth',
+    'gitsigns-blame',
+    'help',
+    'lspinfo',
+    'neotest-output',
+    'neotest-output-panel',
+    'neotest-summary',
+    'notify',
+    'qf',
+    'startuptime',
+    'tsplayground',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set('n', '<Esc>', function()
+        vim.cmd 'close'
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = 'Quit buffer',
+      })
+    end)
+  end,
+})
+
 -- highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup 'highlight_yank',
