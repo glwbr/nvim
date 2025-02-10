0ccb96d2f3b816f6ee0f1b96c3a973c50fae63b5
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-02-10 16:13:17 -0300

    refactor(harpoon): use Ctrl instead of Alt

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,4 +1,5 @@
 local utils = require 'utils'
+local extensions = require 'extensions.harpoon'
 local map = utils.map
 
 return {
@@ -8,61 +9,37 @@ return {
   config = function()
     local harpoon = require 'harpoon'
 
-    harpoon:setup()
-    local conf = require('telescope.config').values
-
-    local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-      end
-
-      require('telescope.pickers')
-        .new({}, {
-          prompt_title = 'Harpoon',
-          finder = require('telescope.finders').new_table {
-            results = file_paths,
-          },
-          previewer = conf.file_previewer {},
-          sorter = conf.generic_sorter {},
-          theme = 'dropdown',
-          layout_config = {
-            height = 0.3,
-          },
-        })
-        :find()
-    end
+    harpoon:setup {}
 
     map('n', '<leader>a', function()
       harpoon:list():add()
     end)
 
-    map('n', '<A-e>', function()
-      toggle_telescope(harpoon:list())
+    map('n', '<C-e>', function()
+      extensions.toggle_telescope(harpoon:list())
     end, { desc = 'Harpoon Quick Menu' })
 
-    map('n', '<A-1>', function()
+    map('n', '<C-1>', function()
       harpoon:list():select(1)
     end, { desc = 'Goto Harpoon File 1' })
 
-    map('n', '<A-2>', function()
+    map('n', '<C-2>', function()
       harpoon:list():select(2)
     end, { desc = 'Goto Harpoon File 2' })
 
-    map('n', '<A-3>', function()
+    map('n', '<C-3>', function()
       harpoon:list():select(3)
     end, { desc = 'Goto Harpoon File 3' })
 
-    map('n', '<A-4>', function()
+    map('n', '<C-4>', function()
       harpoon:list():select(4)
     end, { desc = 'Goto Harpoon File 4' })
 
-    map('n', '<A-S-P>', function()
+    map('n', '<C-S-P>', function()
       harpoon:list():prev()
     end)
 
-    map('n', '<A-S-N>', function()
+    map('n', '<C-S-N>', function()
       harpoon:list():next()
     end)
   end,
