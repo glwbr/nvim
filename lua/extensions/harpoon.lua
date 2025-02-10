7f0c7952ab372f083d3ff3222840ae190c650059
Glauber Santana <glauber.silva14@gmail.com>
2025-02-10 16:13:17 -0300

    refactor(harpoon): use Ctrl instead of Alt

---
+++ lua/extensions/harpoon.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.toggle_telescope = function(harpoon_files)
+  local file_paths = {}
+  local conf = require('telescope.config').values
+
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = require('telescope.finders').new_table {
+        results = file_paths,
+      },
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+    })
+    :find()
+end
+
+return M
