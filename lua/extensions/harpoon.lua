4d10c5b1ef8f8236e5756aa48e9e95b9383ec809
glwbr <glauber.silva14@gmail.com>
2025-02-15 12:08:15 -0300

    fix(extensions): not able to delete entries from telescope-ui

--- lua/extensions/harpoon.lua
+++ lua/extensions/harpoon.lua
@@ -1,24 +1,39 @@
 local M = {}
 
 M.toggle_telescope = function(harpoon_files)
-  local file_paths = {}
-  local conf = require('telescope.config').values
+  local telescope = require 'telescope.config'
+  local finders = require 'telescope.finders'
 
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
+  local finder = function()
+    local paths = {}
+
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return finders.new_table { results = paths }
   end
 
   require('telescope.pickers')
     .new({}, {
       prompt_title = 'Harpoon',
-      finder = require('telescope.finders').new_table {
-        results = file_paths,
-      },
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
+      finder = finder(),
+      sorter = telescope.values.generic_sorter {},
       layout_config = {
-        height = 0.5,
+        height = 0.4,
+        width = 0.5,
       },
+      attach_mappings = function(prompt_bufnr, map)
+        map('n', 'dd', function()
+          local state = require 'telescope.actions.state'
+          local selected_entry = state.get_selected_entry()
+          local current_picker = state.get_current_picker(prompt_bufnr)
+
+          table.remove(harpoon_files.items, selected_entry.index)
+          current_picker:refresh(finder())
+        end)
+        return true
+      end,
     })
     :find()
 end
