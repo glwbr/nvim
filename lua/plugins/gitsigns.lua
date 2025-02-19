6a5be57836d1fda501a202e9d7465d0da5378a07
glwbr <glauber.silva14@gmail.com>
2025-02-19 13:55:48 -0300

    feat(gitsigns): enable blame_line popup

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,6 +13,13 @@ return {
     },
     current_line_blame_formatter = '<author> • <author_time:%R> <summary>',
     current_line_blame_formatter_nc = 'Uncommitted changes',
+    preview_config = {
+      border = 'none',
+      style = 'minimal',
+      relative = 'cursor',
+      row = 0,
+      col = 1,
+    },
     signs = {
       add = { text = '│' },
       change = { text = '│' },
@@ -40,14 +47,15 @@ return {
         end
       end, { desc = 'Jump to previous git change' })
 
+      map('n', '<leader>gb', gs.blame_line, { desc = 'Blame line' })
       map('n', '<leader>gD', gs.diffthis, { desc = 'Diff' })
       map('n', '<leader>gp', gs.preview_hunk_inline, { desc = 'Preview changes' })
-      map('v', '<leader>gu', function()
-        gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-      end, { desc = 'Undo staged hunk' })
       map('v', '<leader>gs', function()
         gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
       end, { desc = 'Stage selected hunk' })
+      map('v', '<leader>gu', function()
+        gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = 'Undo staged hunk' })
     end,
   },
 }
