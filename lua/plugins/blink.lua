8d7c08c0fe0e685c1ba8cf0cd462acc906d18ebb
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-03-04 00:22:59 -0300

    refactor(plugins): simplify blink.cmp config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,43 +2,27 @@ return {
   'saghen/blink.cmp',
   dependencies = 'rafamadriz/friendly-snippets',
   version = '*',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'normal',
-    },
+    keymap = { preset = 'default' },
+    appearance = { nerd_font_variant = 'normal' },
     completion = {
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 500,
         update_delay_ms = 50,
         treesitter_highlighting = true,
-        window = {
-          min_width = 10,
-          max_width = 80,
-          max_height = 20,
-          border = 'padded',
-          winblend = 0,
-          winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
-          scrollbar = true,
-          direction_priority = {
-            menu_north = { 'e', 'w', 'n', 's' },
-            menu_south = { 'e', 'w', 's', 'n' },
-          },
-        },
       },
-      ghost_text = {
-        enabled = true,
-      },
-      list = {
-        max_items = 30,
-      },
+      ghost_text = { enabled = true },
+      list = { max_items = 30 },
     },
-    keymap = { preset = 'default' },
-    cmdline = { enabled = false },
+
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
+
+    fuzzy = { implementation = 'rust' },
   },
   opts_extend = { 'sources.default' },
 }
