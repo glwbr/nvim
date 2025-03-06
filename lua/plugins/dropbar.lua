91b8dbb4e2f65ca9d272dea3a552c8e001c40226
glwbr <glauber.silva14@gmail.com>
2025-03-06 15:47:50 -0300

    fix(plugins): enable dropbar

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,6 +1,5 @@
 return {
   'Bekaboo/dropbar.nvim',
-  enabled = false,
   dependencies = { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
   config = function()
     require('dropbar').setup {
