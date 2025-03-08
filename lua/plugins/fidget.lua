b91b1357dd66f3a5264021b2bff47935e2697b8b
glwbr <glauber.silva14@gmail.com>
2025-03-07 22:18:32 -0300

    feat(plugins): add ui.lua and mini.icons

--- lua/plugins/fidget.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'j-hui/fidget.nvim',
-  opts = { notification = { window = { winblend = 0 } } },
-  config = function(_, opts)
-    require('fidget').setup(opts)
-  end,
-}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,31 @@
+local utils = require 'utils'
+local map = utils.map
+
+return {
+  {
+    'j-hui/fidget.nvim',
+    event = 'LspAttach',
+    opts = { notification = { window = { winblend = 0 } } },
+    config = function(_, opts)
+      require('fidget').setup(opts)
+    end,
+  },
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      file = {
+        ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
+        ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+      },
+      filetype = {
+        dotenv = { glyph = '', hl = 'MiniIconsYellow' },
+      },
+    },
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
+    end,
+  },
+}
