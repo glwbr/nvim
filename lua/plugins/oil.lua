b233e674e172b5642ec18ab24411366d1f425c90
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-03-07 22:24:08 -0300

    style(oil): rm extra table

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,12 @@
 return {
-  {
-    'stevearc/oil.nvim',
-    lazy = false,
-    config = function()
-      ---@module 'oil'
-      ---@type oil.SetupOpts
-      require('oil').setup {
-        skip_confirm_for_simple_edits = true,
-        view_options = { show_hidden = true },
-      }
-    end,
-  },
+  'stevearc/oil.nvim',
+  lazy = false,
+  config = function()
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    require('oil').setup {
+      skip_confirm_for_simple_edits = true,
+      view_options = { show_hidden = true },
+    }
+  end,
 }
