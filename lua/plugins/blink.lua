65cfcb2d018a0f3ba589d221f08324524be54887
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-03-11 16:00:12 -0300

    feat(plugins/blink): add LuaSnip and mini.icons

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,39 @@
 return {
   'saghen/blink.cmp',
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    {
+      'L3MON4D3/LuaSnip',
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+  },
   version = '*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = { preset = 'default' },
     appearance = { nerd_font_variant = 'normal' },
+    keymap = { preset = 'default' },
+    snippets = { preset = 'luasnip' },
     completion = {
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              ellipsis = false,
+              text = function(ctx)
+                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                return kind_icon
+              end,
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 500,
