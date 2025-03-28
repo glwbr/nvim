171b30e2ac5126aa8b673a7e423ddcf2ce757c45
Glauber S. Santana <hello@glwbr.me>
2025-03-28 14:24:07 -0300

    style: formatting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,7 +2,9 @@ return {
   {
     'j-hui/fidget.nvim',
     event = 'LspAttach',
-    opts = { notification = { window = { winblend = 0 } } },
+    opts = {
+      notification = { window = { winblend = 0 } },
+    },
     config = function(_, opts)
       require('fidget').setup(opts)
     end,
@@ -14,9 +16,7 @@ return {
         ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
         ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
       },
-      filetype = {
-        dotenv = { glyph = '', hl = 'MiniIconsYellow' },
-      },
+      filetype = { dotenv = { glyph = '', hl = 'MiniIconsYellow' } },
     },
     init = function()
       package.preload['nvim-web-devicons'] = function()
