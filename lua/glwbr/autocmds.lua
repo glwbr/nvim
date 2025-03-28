f690e5919ea0fd5ed9c57566cca7eee71324aaab
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-03-28 19:07:13 -0300

    feat(autocmds): add oil, help and eslint cmds

--- lua/glwbr/autocmds.lua
+++ lua/glwbr/autocmds.lua
@@ -1,9 +1,9 @@
 local api = vim.api
-
 local autocmd = api.nvim_create_autocmd
+local usercmd = api.nvim_create_user_command
 
 local function augroup(name)
-  return api.nvim_create_augroup('glwbr_' .. name, { clear = true })
+  return api.nvim_create_augroup('user_' .. name, { clear = true })
 end
 
 autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
@@ -13,7 +13,7 @@ autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
       vim.cmd 'checktime'
     end
   end,
-  desc = 'Reload the file when changed',
+  desc = 'Reload the file when changed externally',
 })
 
 autocmd('FileType', {
@@ -48,7 +48,7 @@ autocmd('FileType', {
   desc = 'Close certain buffers with <Esc>',
 })
 
-autocmd('TextYankPost', {
+autocmd({ 'TextYankPost' }, {
   group = augroup 'highlight_yank',
   callback = function()
     (vim.hl or vim.highlight).on_yank { timeout = 50 }
@@ -75,10 +75,11 @@ autocmd({ 'BufWritePre' }, {
     local file = vim.uv.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
+  desc = 'Create parent directories when saving new files',
 })
 
-autocmd('BufReadPost', {
-  group = augroup 'last_loc',
+autocmd({ 'BufReadPost' }, {
+  group = augroup 'last_location',
   callback = function(event)
     local exclude = { 'gitcommit' }
     local buf = event.buf
@@ -92,16 +93,47 @@ autocmd('BufReadPost', {
       pcall(api.nvim_win_set_cursor, 0, mark)
     end
   end,
-  desc = 'Go to last loc when opening a buffer',
+  desc = 'Go to last location when opening a buffer',
 })
 
-vim.api.nvim_create_autocmd('User', {
-  group = augroup 'move_file',
+autocmd({ 'User' }, {
+  group = augroup 'oil',
   pattern = 'OilActionsPost',
   callback = function(event)
     if event.data.actions.type == 'move' then
       Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
     end
   end,
-  desc = 'Go to last loc when opening a buffer',
+  desc = 'Handle file renaming in Oil.nvim with Snacks.rename',
 })
+
+autocmd({ 'FileType' }, {
+  group = augroup 'vertical_help',
+  pattern = 'help',
+  callback = function()
+    vim.bo.bufhidden = 'unload'
+    vim.cmd.wincmd 'L'
+    vim.cmd.wincmd '='
+  end,
+  desc = 'Open help in a vertical split',
+})
+
+autocmd({ 'FileType' }, {
+  group = augroup 'oil',
+  pattern = 'oil',
+  callback = function()
+    vim.opt_local.colorcolumn = ''
+  end,
+  desc = 'Hide colorcolumn when  browsing files',
+})
+
+usercmd('EslintFix', function()
+  if vim.fn.executable 'eslint_d' ~= 1 then
+    vim.notify('eslint_d not found in PATH', vim.log.levels.ERROR)
+    return
+  end
+
+  local cursor_pos = vim.fn.getpos '.'
+  vim.cmd '%!eslint_d --stdin --fix-to-stdout --stdin-filename %'
+  vim.fn.setpos('.', cursor_pos)
+end, { desc = 'Fix ESLint issues in current buffer' })
