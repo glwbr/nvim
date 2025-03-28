b3cd285135b7f7beaacf71df061fa9bf78d68529
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-03-28 19:06:40 -0300

    feat(colorschemes): add custom highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,36 @@ return {
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    enabled = false,
+    enabled = true,
     opts = {
       variant = 'main',
       styles = {
-        bold = true,
-        italic = true,
+        bold = false,
+        italic = false,
         transparency = false,
+      },
+      highlight_groups = {
+        SnacksPicker = { fg = 'subtle', bg = 'base' },
+        SnacksPickerTitle = { fg = 'base', bg = 'foam' },
+        SnacksPickerBorder = { fg = 'base', bg = 'base' },
+        SnacksPickerPreviewVisual = { fg = 'subtle', bg = 'love' },
+        SnacksPickerInput = { fg = 'subtle', bg = 'base' },
+        SnacksPickerInputTitle = { fg = 'foam', bg = 'base' },
+        SnacksPickerInputBorder = { fg = 'base', bg = 'base' },
+        SnacksPickerPreview = { fg = 'subtle', bg = 'surface' },
+        SnacksPickerPreviewTitle = { fg = 'surface', bg = 'iris' },
+        SnacksPickerPreviewBorder = { fg = 'surface', bg = 'surface' },
+        SnacksPickerBoxTitle = { fg = 'base', bg = 'gold' },
+        SnacksPickerBoxBorder = { fg = 'base', bg = 'base' },
+
+        BlinkCmpMenu = { bg = 'base' },
+        BlinkCmpMenuBorder = { fg = 'foam' },
+        BlinkCmpDoc = { bg = 'base' },
+        BlinkCmpDocBorder = { fg = 'foam' },
+        BlinkCmpSignatureHelp = { bg = 'base' },
+        BlinkCmpSignatureHelpBorder = { fg = 'foam' },
+        BlinkCmpDocSeparator = { fg = 'foam', bg = 'base' },
+        BlinkCmpGhostText = { fg = 'muted' },
       },
     },
     config = function(_, opts)
@@ -20,17 +43,18 @@ return {
   },
   {
     'catppuccin/nvim',
-    enabled = true,
+    enabled = false,
     name = 'catppuccin',
     priority = 1000,
     config = function()
       require('catppuccin').setup {
-        background = {
-          light = 'latte',
-          dark = 'mocha',
-        },
+        background = { light = 'latte', dark = 'mocha' },
         flavour = 'mocha',
         integrations = {
+          blink_cmp = true,
+          fidget = true,
+          gitsigns = true,
+          harpoon = true,
           native_lsp = {
             enabled = true,
             underlines = {
@@ -40,7 +64,35 @@ return {
               information = { 'undercurl' },
             },
           },
+          snacks = { enabled = true, indent_scope_color = 'mauve' },
+          telescope = true,
+          treesitter = true,
         },
+        custom_highlights = function(colors)
+          return {
+            SnacksPicker = { fg = colors.overlay2, bg = colors.mantle },
+            SnacksPickerTitle = { fg = colors.mantle, bg = colors.blue },
+            SnacksPickerBorder = { fg = colors.mantle, bg = colors.mantle },
+            SnacksPickerPreviewVisual = { fg = colors.overlay2, bg = colors.red },
+            SnacksPickerInput = { fg = colors.overlay2, bg = colors.mantle },
+            SnacksPickerInputTitle = { fg = colors.mantle, bg = colors.peach },
+            SnacksPickerInputBorder = { fg = colors.mantle, bg = colors.mantle },
+            SnacksPickerPreview = { fg = colors.overlay2, bg = colors.crust },
+            SnacksPickerPreviewTitle = { fg = colors.crust, bg = colors.mauve },
+            SnacksPickerPreviewBorder = { fg = colors.crust, bg = colors.crust },
+            SnacksPickerBoxTitle = { fg = colors.mantle, bg = colors.peach },
+            SnacksPickerBoxBorder = { fg = colors.mantle, bg = colors.mantle },
+
+            BlinkCmpMenu = { bg = colors.base },
+            BlinkCmpMenuBorder = { fg = colors.blue },
+            BlinkCmpDoc = { bg = colors.base },
+            BlinkCmpDocBorder = { fg = colors.blue },
+            BlinkCmpSignatureHelp = { bg = colors.base },
+            BlinkCmpSignatureHelpBorder = { fg = colors.blue },
+            BlinkCmpDocSeparator = { fg = colors.blue, bg = colors.base },
+            BlinkCmpGhostText = { fg = colors.overlay2 },
+          }
+        end,
         styles = {
           comments = { 'italic' },
           conditionals = {},
@@ -57,6 +109,7 @@ return {
         },
         term_colors = true,
       }
+
       vim.cmd 'colorscheme catppuccin'
     end,
   },
