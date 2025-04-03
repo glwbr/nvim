63047087aebf12da3aca170eb6c8ad553126cca2
Glauber S. Santana <hello@glwbr.me>
2025-04-03 03:32:15 -0300

    style: adjust separator char

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,7 +10,7 @@ return {
   opts = {
     current_line_blame = true,
     current_line_blame_opts = { delay = 500 },
-    current_line_blame_formatter = '<author> • <author_time:%R> - <summary>',
+    current_line_blame_formatter = '<author> ∙ <author_time:%R> - <summary>',
     current_line_blame_formatter_nc = 'Uncommitted changes',
     numhl = false,
     signs = {
