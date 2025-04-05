4fff6fe1691ed3d793561be11f36f49ad5254fd4
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-04-04 21:54:24 -0300

    refactor: modularize flake.nix configuration into separate nix files

--- flake.nix
+++ flake.nix
@@ -14,154 +14,17 @@
     {
       nixpkgs,
       nixCats,
-      incline-nvim,
       ...
     }@inputs:
     let
       inherit (nixCats) utils;
       luaPath = "${./.}";
       forEachSystem = utils.eachSystem nixpkgs.lib.platforms.all;
-      # extra_pkg_config.allowUnfree = true;
-      dependencyOverlays = [
-        (utils.standardPluginOverlay inputs)
-        (final: prev: {
-          vimPlugins = prev.vimPlugins // {
-            incline-nvim = prev.vimUtils.buildVimPlugin {
-              name = "incline.nvim";
-              src = incline-nvim;
-            };
-          };
-        })
-      ];
 
-      categoryDefinitions =
-        { pkgs, ... }:
-        {
-          sharedLibraries = { };
-          environmentVariables = { };
-          extraLuaPackages = { };
-          python3.libraries = { };
-          extraWrapperArgs = { };
-
-          lspsAndRuntimeDeps = with pkgs; {
-            core = [
-              universal-ctags
-              ripgrep
-              fd
-              jq
-              wl-clipboard
-            ];
-            languages = {
-              go = {
-                debug = [ delve ];
-                lint = [ golangci-lint ];
-              };
-              docker = {
-                lint = [ hadolint ];
-              };
-              lua = [
-                lua-language-server
-                selene
-                stylua
-              ];
-              nix = [
-                nix
-                nixd
-                nixfmt-rfc-style
-              ];
-              web = [
-                vtsls
-                tailwindcss-language-server
-                markdownlint-cli
-                prettierd
-                nodePackages.jsonlint
-                eslint_d
-                vscode-langservers-extracted
-              ];
-            };
-          };
-
-          startupPlugins = with pkgs.vimPlugins; {
-            core = [
-              blink-cmp
-              conform-nvim
-              lazy-nvim
-              {
-                plugin = luasnip;
-                name = "LuaSnip";
-              }
-              nvim-lspconfig
-              nvim-treesitter.withAllGrammars
-              plenary-nvim
-            ];
-
-            navigationAndWorkflow = [
-              {
-                plugin = harpoon2;
-                name = "harpoon";
-              }
-              tmux-navigator
-              oil-nvim
-            ];
-
-            qualityOfLife = [
-              fidget-nvim
-              {
-                plugin = incline-nvim;
-                name = "incline";
-              }
-              snacks-nvim
-              gitsigns-nvim
-              nvim-lint
-              todo-comments-nvim
-            ];
-
-            pluginUtilities = [
-              friendly-snippets
-              lazydev-nvim
-            ];
-
-            ui = [
-              {
-                plugin = catppuccin-nvim;
-                name = "catppuccin";
-              }
-              mini-icons
-              {
-                plugin = rose-pine;
-                name = "rose-pine";
-              }
-            ];
-          };
-
-          optionalPlugins = { };
-        };
-
-      packageDefinitions = {
-        nvim =
-          { ... }:
-          {
-            settings = {
-              wrapRc = true;
-              aliases = [ "v" ];
-              hosts.node.enable = false;
-              hosts.python3.enable = false;
-              hosts.ruby.enable = false;
-            };
-
-            categories = {
-              core = true;
-              languages = true;
-
-              navigationAndWorkflow = true;
-              qualityOfLife = true;
-              pluginUtilities = true;
-              ui = true;
-
-              have_nerd_font = true;
-            };
-          };
-      };
+      # Import modularized components
+      overlays = import ./nix/overlays.nix { inherit utils inputs; };
+      categoryDefinitions = import ./nix/categories.nix;
+      packageDefinitions = import ./nix/packages.nix;
       defaultPackageName = "nvim";
     in
     forEachSystem (
@@ -170,7 +33,8 @@
         inherit (utils) baseBuilder;
         nixCatsBuilder = baseBuilder luaPath {
           inherit nixpkgs;
-          inherit system dependencyOverlays;
+          inherit system;
+          dependencyOverlays = overlays;
         } categoryDefinitions packageDefinitions;
         defaultPackage = nixCatsBuilder defaultPackageName;
         pkgs = import nixpkgs { inherit system; };
@@ -193,20 +57,20 @@
         inherit
           categoryDefinitions
           defaultPackageName
-          dependencyOverlays
           luaPath
           packageDefinitions
           ;
+        dependencyOverlays = overlays;
       };
       homeModule = utils.mkHomeModules {
         inherit nixpkgs;
         inherit
           categoryDefinitions
           defaultPackageName
-          dependencyOverlays
           luaPath
           packageDefinitions
           ;
+        dependencyOverlays = overlays;
       };
     };
 }
