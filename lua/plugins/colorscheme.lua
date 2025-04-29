a28ad6b23b7161648d6643a60b7c7f9382df2c93
Glauber Santana <hello@glwbr.me>
2025-04-29 20:22:26 -0300

    refactor(colorscheme): settle on rosé-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,41 @@
-local catUtils = require 'utils.cats'
+-- local catUtils = require 'utils.cats'
 
 return {
   {
     'rose-pine/neovim',
-    enabled = false,
     name = 'rose-pine',
+    enabled = true,
+    lazy = false,
     priority = 1000,
     opts = {
-      variant = 'main',
-      styles = {
-        bold = false,
-        italic = false,
-        transparency = false,
+      variant = 'auto',
+      dark_variant = 'main',
+      dim_inactive_windows = false,
+      extend_background_behind_borders = true,
+
+      enable = {
+        terminal = true,
+        legacy_highlights = true,
+        migrations = true,
       },
+
+      styles = {
+        bold = true,
+        italic = false,
+        transparency = true,
+      },
+
       highlight_groups = {
+        ColorColumn = { bg = 'highlight_low' },
 
         SnacksPicker = { fg = 'subtle', bg = 'NONE' },
         SnacksPickerInput = { fg = 'subtle', bg = 'NONE' },
         SnacksPickerPreview = { fg = 'subtle', bg = 'NONE' },
 
-        BlinkCmpMenu = { bg = 'base' },
-        BlinkCmpMenuBorder = { fg = 'foam' },
-        BlinkCmpDoc = { bg = 'base' },
-        BlinkCmpDocBorder = { fg = 'foam' },
+        BlinkCmpMenu = { bg = 'NONE' },
+        BlinkCmpMenuBorder = { fg = 'muted' },
+        BlinkCmpDoc = { bg = 'NONE' },
+        BlinkCmpDocBorder = { fg = 'muted' },
         BlinkCmpSignatureHelp = { bg = 'base' },
         BlinkCmpSignatureHelpBorder = { fg = 'foam' },
         BlinkCmpDocSeparator = { fg = 'foam', bg = 'base' },
@@ -32,56 +45,6 @@ return {
     config = function(_, opts)
       require('rose-pine').setup(opts)
       vim.cmd 'colorscheme rose-pine'
-    end,
-  },
-  {
-    'catppuccin/nvim',
-    enabled = true,
-    name = 'catppuccin',
-    priority = 1000,
-    opts = {
-      background = { light = 'latte', dark = 'mocha' },
-      flavour = 'mocha',
-      term_colors = true,
-      transparent_background = true,
-      integrations = {
-        blink_cmp = true,
-        fidget = true,
-        gitsigns = true,
-        harpoon = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { 'undercurl' },
-            hints = { 'undercurl' },
-            warnings = { 'undercurl' },
-            information = { 'undercurl' },
-          },
-        },
-        snacks = { enabled = true, indent_scope_color = 'mauve' },
-        treesitter = true,
-      },
-      custom_highlights = function(colors)
-        return {
-
-          SnacksPicker = { fg = colors.overlay2, bg = 'NONE' },
-          SnacksPickerInput = { fg = colors.overlay2, bg = 'NONE' },
-          SnacksPickerPreview = { fg = colors.overlay2, bg = 'NONE' },
-
-          BlinkCmpMenu = { bg = colors.base },
-          BlinkCmpMenuBorder = { fg = colors.blue },
-          BlinkCmpDoc = { bg = colors.base },
-          BlinkCmpDocBorder = { fg = colors.blue },
-          BlinkCmpSignatureHelp = { bg = colors.base },
-          BlinkCmpSignatureHelpBorder = { fg = colors.blue },
-          BlinkCmpDocSeparator = { fg = colors.blue, bg = colors.base },
-          BlinkCmpGhostText = { fg = colors.overlay2 },
-        }
-      end,
-    },
-    config = function(_, opts)
-      require('catppuccin').setup(opts)
-      vim.cmd 'colorscheme catppuccin'
     end,
   },
 }
