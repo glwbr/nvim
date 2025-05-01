264bb543a7633e64c5d602ee7bf632ffd6c76551
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-01 16:42:10 -0300

    feat(plugins/ai): add copilot and blink-copilot integration

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,8 @@
 return {
   'saghen/blink.cmp',
+  version = '*',
   dependencies = {
+    'fang2hou/blink-copilot',
     'rafamadriz/friendly-snippets',
     {
       'L3MON4D3/LuaSnip',
@@ -9,7 +11,6 @@ return {
       end,
     },
   },
-  version = '*',
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -20,15 +21,20 @@ return {
       menu = {
         border = 'rounded',
         draw = {
-          padding = 0,
-          columns = { { 'kind_icon', gap = 1 }, { gap = 1, 'label' }, { 'kind', gap = 1 } },
+          padding = { 1, 1 },
+          columns = {
+            { 'kind_icon', gap = 1 },
+            { gap = 1, 'label' },
+            { 'kind', gap = 1 },
+          },
           components = {
             kind_icon = {
-              ellipsis = false,
-              text = function(ctx)
-                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                return kind_icon
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
               end,
+            },
+            kind = {
               highlight = function(ctx)
                 local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
                 return hl
@@ -42,14 +48,29 @@ return {
         update_delay_ms = 50,
         auto_show_delay_ms = 500,
         treesitter_highlighting = true,
-        window = { border = 'rounded' },
+        window = { border = 'none' },
       },
-      ghost_text = { enabled = true, show_with_menu = true },
+      ghost_text = { enabled = true, show_with_menu = false },
       list = { max_items = 30 },
     },
-
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-
+    sources = {
+      default = { 'lsp', 'snippets', 'path', 'buffer', 'copilot' },
+      providers = {
+        copilot = {
+          async = true,
+          name = 'copilot',
+          score_offset = 100,
+          module = 'blink-copilot',
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              item.kind_icon = ' '
+              item.kind_name = 'Copilot'
+            end
+            return items
+          end,
+        },
+      },
+    },
     fuzzy = { implementation = 'prefer_rust' },
   },
   opts_extend = { 'sources.default' },
