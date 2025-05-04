5f6b449eb5d667ad77b15df3a103e98464ff12a8
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-04 12:03:03 -0300

    feat(json): auto-add trailing comma on newline in json/jsonc files
    - add `add_trailing_comma_if_needed` from to `utils`
    - add filetype-specific keymaps for `json` and `jsonc`

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -23,4 +23,15 @@ function M.map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+function M.add_trailing_comma_if_needed()
+  local line = vim.api.nvim_get_current_line()
+
+  local should_add_comma = string.find(line, '[^,{[]$')
+  if should_add_comma then
+    return 'A,<cr>'
+  else
+    return 'o'
+  end
+end
+
 return M
