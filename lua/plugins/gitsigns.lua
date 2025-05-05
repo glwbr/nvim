4d1efc0dd52f84e0a23f6fc2822ce833cd04921e
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-05 16:07:03 -0300

    fix(plugins/gitsigns): differentiate topdelete sign

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -17,7 +17,7 @@ return {
       add = { text = '│' },
       change = { text = '│' },
       delete = { text = '│' },
-      topdelete = { text = '│' },
+      topdelete = { text = '‾' },
       changedelete = { text = '│' },
       untracked = { text = '│' },
     },
