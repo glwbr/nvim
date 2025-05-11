ec3a9670d2a2db2ac2c3fbad75c042ec8bba7e00
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-11 17:51:42 -0300

    refactor(utils): move first_available function to conform module

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,20 +2,6 @@
 
 local M = {}
 
----@param bufnr integer
----@param ... string
----@return string
-function M.first(bufnr, ...)
-  local conform = require 'conform'
-  for i = 1, select('#', ...) do
-    local formatter = select(i, ...)
-    if conform.get_formatter_info(formatter, bufnr).available then
-      return formatter
-    end
-  end
-  return select(1, ...)
-end
-
 function M.map(mode, lhs, rhs, opts)
   opts = opts or {}
   opts.noremap = true
