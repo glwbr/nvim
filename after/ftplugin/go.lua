854f1f47afdd3123f53901234e5e3e6b140a3813
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-11 00:25:24 -0300

    feat(ftplugin): refactor filetype settings
    - add gitcommit and markdown wrap and spell settings
    - refactor variable naming in to use consistent 'opt' naming convention
    - remove wrap_spell autocmd in favor of dedicated ftplugin files

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,4 +1,6 @@
-vim.opt_local.expandtab = false
-vim.opt_local.tabstop = 4
-vim.opt_local.shiftwidth = 4
-vim.opt_local.softtabstop = 4
+local opt = vim.opt_local
+
+opt.expandtab = false
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,3 +1,3 @@
-local set = vim.opt_local
+local opt = vim.opt_local
 
-set.shiftwidth = 2
+opt.shiftwidth = 2
