236c1e52226f4105a3c282833e988dc5e3c31c22
Glauber Santana <hello@glwbr.me>
2025-05-11 17:51:42 -0300

    refactor(utils): move first_available function to conform module

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,15 @@
-local utils = require 'utils'
+---@param bufnr integer
+---@param ... string
+---@return string
+local first_available = function(bufnr, ...)
+  for i = 1, select('#', ...) do
+    local formatter = select(i, ...)
+    if require('conform').get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
 
 return {
   'stevearc/conform.nvim',
@@ -7,12 +18,14 @@ return {
   opts = {
     notify_on_error = false,
     log_level = vim.log.levels.DEBUG,
+
     default_format_opts = {
       timeout_ms = 500,
       async = false,
       quiet = false,
       lsp_format = 'fallback',
     },
+
     format_on_save = function(bufnr)
       local disable_filetypes = { c = true, cpp = true }
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
@@ -20,6 +33,7 @@ return {
       end
       return { lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype] }
     end,
+
     formatters = {
       ['biome-check'] = { append_args = { '--unsafe' } },
       prettierd = { require_cwd = true },
@@ -30,6 +44,7 @@ return {
       },
       --['goimports-reviser'] = { prepend_args = { '-rm-unused' } },
     },
+
     formatters_by_ft = {
       ['_'] = { 'trim_whitespace' },
       lua = { 'stylua' },
@@ -42,7 +57,7 @@ return {
       jsonc = { 'prettierd' },
       go = { 'goimports', 'gofumpt' },
       markdown = function(bufnr)
-        return { utils.first(bufnr, 'prettierd', 'prettier'), 'injected' }
+        return { first_available(bufnr, 'prettierd', 'prettier'), 'injected' }
       end,
     },
   },
