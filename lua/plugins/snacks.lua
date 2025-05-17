a0df90829d30041c01dac13118fe948cedd01225
Glauber Santana <6403699+glwbr@users.noreply.github.com >
2025-05-17 19:59:49 -0300

    refactor(plugins/snacks): small adjustments

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,20 +6,19 @@ return {
   opts = {
     words = { enabled = false },
     bigfile = { enabled = true },
-    notifier = { enabled = true, style = 'minimal', top_down = false },
+    notifier = { enable = true, margin = { top = 1, right = 1, bottom = 1 }, top_down = false, style = 'compact' },
+    quickfile = { enabled = true },
+    statuscolumn = { enable = true },
     picker = {
-      enable = true,
+      enabled = true,
       ui_select = true,
-      layout = {
-        layout = {
-          backdrop = false,
-        },
-      },
+      layout = { layout = { backdrop = false } },
       win = {
         preview = {
           wo = {
             foldcolumn = '0',
             number = false,
+            foldenable = false,
             relativenumber = false,
             fillchars = 'eob: ',
             signcolumn = 'no',
@@ -39,12 +38,6 @@ return {
     { '<leader>sg', function() Snacks.picker.git_grep() end, desc = '[S]earch [/] in Open Files' },
     { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp Tags' },
     { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eybindings' },
-    { '<leader>sn',
-      function()
-        Snacks.picker.files { cwd = vim.fn.stdpath 'config', title = 'Neovim Files' }
-      end,
-      desc = '[S]earch [N]eovim Config Files',
-    },
     { '<leader>sp', function() Snacks.picker.projects() end, desc = '[S]earch [P]projects' },
     { '<leader>ss', function() Snacks.picker.pickers() end, desc = '[S]earch [S] Built-in Pickers' },
     { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch Current [W]ord' },
