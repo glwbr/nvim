102dfc254caa68137e944e41e9d24093010dea04
Glauber Santana <6403699+glwbr@users.noreply.github.com>
2026-04-02 01:23:50 -0300

    feat(snacks): update picker layout and dashboard config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,34 +6,38 @@ return {
   opts = {
     words = { enabled = false },
     bigfile = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    notifier = { enabled = true, margin = { top = 1, right = 1, bottom = 1 }, top_down = false, style = 'compact' },
     dashboard = {
       enabled = true,
       preset = {
         ---@type snacks.dashboard.Item[]
         -- stylua: ignore start
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.files({filter = {cwd = true}, layout = 'default'})" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "s", desc = "Find Text", action = ":lua Snacks.picker.grep({layout = 'default'})" },
-          { icon = " ", key = "b", desc = "File browser", action = function()  require("yazi").yazi(nil, vim.fn.getcwd()) end},
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.picker.files({filter = {cwd = true}, layout = 'default'})", },
+          { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+          { icon = ' ', key = 's', desc = 'Find Text', action = ":lua Snacks.picker.grep({layout = 'default'})" },
+          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
           -- stylua: ignore end
         },
       },
       sections = {
         { section = 'header' },
         { section = 'keys', gap = 1 },
-        { title = 'Recent Files', section = 'recent_files', cwd = true },
+        { padding = 1 },
+        { section = 'recent_files', cwd = true },
+        { padding = 1 },
         { section = 'startup' },
       },
     },
-    notifier = { enable = true, margin = { top = 1, right = 1, bottom = 1 }, top_down = false, style = 'compact' },
-    quickfile = { enabled = true },
-    statuscolumn = { enable = true },
     picker = {
       enabled = true,
       ui_select = true,
-      layout = { layout = { backdrop = false } },
+      layout = {
+        preset = 'telescope',
+        layout = { backdrop = false },
+      },
       win = {
         preview = {
           wo = {
@@ -50,19 +54,19 @@ return {
   },
   keys = {
     -- stylua: ignore start
-    { '<leader>/', function() Snacks.picker.grep_buffers() end, desc = ' Fuzzily search in current buffer' },
-    { '<leader>s.', function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
-    { '<leader>ff', function() Snacks.picker.files() end, desc = '[S]earch [F]iles' },
-    { '<leader>rf', function() Snacks.rename.rename_file() end, desc = 'Rename File' },
-    { '<leader>sb', function() Snacks.picker.buffers() end, desc = '[ ] Find Existing Buffers' },
-    { '<leader>sd', function() Snacks.picker.diagnostics() end, desc = '[S]earch [D]iagnostics' },
-    { '<leader>sf', function() Snacks.picker.smart() end, desc = '[S]earch [F]iles' },
-    { '<leader>sg', function() Snacks.picker.git_grep() end, desc = '[S]earch [/] in Open Files' },
-    { '<leader>sh', function() Snacks.picker.help() end, desc = '[S]earch [H]elp Tags' },
-    { '<leader>sk', function() Snacks.picker.keymaps() end, desc = '[S]earch [K]eybindings' },
-    { '<leader>sp', function() Snacks.picker.projects() end, desc = '[S]earch [P]projects' },
-    { '<leader>ss', function() Snacks.picker.pickers() end, desc = '[S]earch [S] Built-in Pickers' },
-    { '<leader>sw', function() Snacks.picker.grep_word() end, desc = '[S]earch Current [W]ord' },
+    { '<leader>/',          function() Snacks.picker.grep_buffers() end,   desc = 'Grep Current Buffer' },
+    { '<leader><leader>',   function() Snacks.picker.buffers() end,        desc = 'Find Buffers' },
+    { '<leader>s.',         function() Snacks.picker.recent() end,         desc = 'Recent Files' },
+    { '<leader>sf',         function() Snacks.picker.smart({ filter = { cwd = true } }) end, desc = 'Search Files' },
+    { '<leader>sg',         function() Snacks.picker.grep() end,           desc = 'Search Grep' },
+    { '<leader>sb',         function() Snacks.picker.buffers() end,        desc = 'Search Buffers' },
+    { '<leader>sd',         function() Snacks.picker.diagnostics() end,    desc = 'Search Diagnostics' },
+    { '<leader>sh',         function() Snacks.picker.help() end,           desc = 'Search Help' },
+    { '<leader>sk',         function() Snacks.picker.keymaps() end,        desc = 'Search Keymaps' },
+    { '<leader>sp',         function() Snacks.picker.projects() end,       desc = 'Search Projects' },
+    { '<leader>ss',         function() Snacks.picker.pickers() end,        desc = 'Search Pickers' },
+    { '<leader>sw',         function() Snacks.picker.grep_word() end,      desc = 'Search Word' },
+    { '<leader>rf',         function() Snacks.rename.rename_file() end,    desc = 'Rename File' },
     -- stylua: ignore end
   },
 }
